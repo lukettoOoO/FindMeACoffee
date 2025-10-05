2a8aa2ced7c6a48be899267d8f14610754af93c8
lukettoOoO <110674888+lukettoOoO@users.noreply.github.com>
2025-10-05 17:44:41 +0300

    Initial commit

---
+++ src/main/java/com/findmeacoffee/FindmeacoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.findmeacoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmeacoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmeacoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findmeacoffee/FindmeacoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmeacoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmeacoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
